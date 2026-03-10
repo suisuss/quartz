826b585d4f6ac0be288373d33a975494f428fd70
Jacob Sussmilch <suisuss@protonmail.com>
2026-03-10 23:12:37 +1100

    fix: correct baseUrl to suisuss.github.io/quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "jacobsussmilch.github.io/quartz",
+    baseUrl: "suisuss.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
